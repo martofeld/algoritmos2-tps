ef25f9823c85eddaba42f657c84d540f7f7523d1
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-09-18 22:32:27 -0300

    Agrego el iterador externo

--- lista_enlazada/lista.c
+++ lista_enlazada/lista.c
@@ -127,3 +127,61 @@ void lista_iterar(lista_t *lista, bool visitar(void *dato, void *extra), void *e
     }
 }
 // --------- END LISTA ---------
+
+// --------- ITERADOR ---------
+struct lista_iter {
+    lista_t *lista;
+    nodo_t *anterior;
+    nodo_t *actual;
+};
+
+lista_iter_t *lista_iter_crear(lista_t *lista) {
+    lista_iter_t *iterador = malloc(sizeof(lista_iter_t));
+    if (!iterador) return NULL;
+
+    iterador->lista = lista;
+    iterador->actual = lista->primero;
+    iterador->anterior = NULL;
+    return iterador;
+}
+
+bool lista_iter_avanzar(lista_iter_t *iter) {
+    iter->anterior = iter->actual;
+    iter->actual = iter->actual->proximo;
+    return true;
+}
+
+void *lista_iter_ver_actual(const lista_iter_t *iter) {
+    if (!iter->actual) return NULL;
+    return iter->actual->dato;
+}
+
+bool lista_iter_al_final(const lista_iter_t *iter) {
+    return !iter->actual;
+}
+
+void lista_iter_destruir(lista_iter_t *iter) {
+    free(iter);
+}
+
+bool lista_iter_insertar(lista_iter_t *iter, void *dato) {
+    nodo_t *nodo = nodo_crear(dato, iter->actual);
+    if (!nodo) return false;
+
+    iter->anterior->proximo = nodo;
+    iter->actual = nodo;
+    iter->lista->cantidad++;
+    return true;
+}
+
+void *lista_iter_borrar(lista_iter_t *iter) {
+    nodo_t *destruir = iter->actual;
+    iter->actual = iter->actual->proximo;
+    iter->anterior->proximo = iter->actual;
+
+    void *valor = destruir->dato;
+    nodo_destruir(destruir);
+    iter->lista->cantidad--;
+    return valor;
+}
+// --------- END ITERADOR ---------
--- lista_enlazada/lista.h
+++ lista_enlazada/lista.h
@@ -20,3 +20,18 @@ void lista_destruir(lista_t *lista, void destruir_dato(void *));
 void lista_iterar(lista_t *lista, bool visitar(void *dato, void *extra), void *extra);
 
 #endif //LISTA_H
+
+#ifndef LISTA_ITERADOR_H
+#define LISTA_ITERADOR_H
+
+typedef struct lista_iter lista_iter_t;
+
+lista_iter_t *lista_iter_crear(lista_t *lista);
+bool lista_iter_avanzar(lista_iter_t *iter);
+void *lista_iter_ver_actual(const lista_iter_t *iter);
+bool lista_iter_al_final(const lista_iter_t *iter);
+void lista_iter_destruir(lista_iter_t *iter);
+bool lista_iter_insertar(lista_iter_t *iter, void *dato);
+void *lista_iter_borrar(lista_iter_t *iter);
+
+#endif //LISTA_ITERADOR_H
