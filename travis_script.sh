5e8529a50f739c98826389c2f4acec12664a8df4
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-09-26 17:38:01 -0300

    Dejo de agregar testing.c

--- travis_script.sh
+++ travis_script.sh
@@ -6,7 +6,7 @@ do
     echo "CORRIENDO PRUEBAS DE ${file}"
     cp testing.c $dir/testing.c
     cp testing.h $dir/testing.h
-    gcc -std=c99 -Wall -Wconversion -Wtype-limits -pedantic -Werror -o ${dir}/pruebas ${dir}/*.c testing.c
+    gcc -std=c99 -Wall -Wconversion -Wtype-limits -pedantic -Werror -o ${dir}/pruebas ${dir}/*.c
     valgrind --leak-check=full --track-origins=yes --show-reachable=yes --error-exitcode=1 ${dir}/pruebas
     ${dir}/pruebas
     rm $dir/testing.c
