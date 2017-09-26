04687c68300da8ccec8f13088f3b38c98fdf4390
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-09-26 17:32:20 -0300

    Arreglo el script de travis

--- travis_script.sh
+++ travis_script.sh
@@ -4,11 +4,11 @@ do
     dir=${dir%*/}
     file=`echo "${dir##*/}" | tr '[a-z]' '[A-Z]'`
     echo "CORRIENDO PRUEBAS DE ${file}"
-    cp ../testing.c testing.c
-    cp ../testing.h testing.h
+    cp testing.c $dir/testing.c
+    cp testing.h $dir/testing.h
     gcc -std=c99 -Wall -Wconversion -Wtype-limits -pedantic -Werror -o ${dir}/pruebas ${dir}/*.c testing.c
     valgrind --leak-check=full --track-origins=yes --show-reachable=yes --error-exitcode=1 ${dir}/pruebas
     ${dir}/pruebas
-    rm testing.c
-    rm testing.h
+    rm $dir/testing.c
+    rm $dir/testing.h
 done
