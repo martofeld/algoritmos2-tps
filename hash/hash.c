aac3e9fb1385ca1b035ac0a19aab466c9c4dbdd8
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-10-05 15:30:14 -0300

    Primera implementacion de hash borrar y hash obtener

--- hash/hash.c
+++ hash/hash.c
@@ -6,12 +6,12 @@
 #include "lista.h"
 #include <stdlib.h>
 
-#define CAPACIDAD_INICIAL 10;
+#define CAPACIDAD_INICIAL 10
 
 typedef struct hash_item {
     char *clave;
     void *valor;
-};
+} hash_item_t;
 
 struct hash {
     lista_t **tabla;
@@ -20,8 +20,7 @@ struct hash {
     hash_destruir_dato_t destruir_dato;
 };
 
-size_t fnv1a_hash(const char* cp)
-{
+size_t fnv1a_hash(const char *cp) {
     size_t hash = 0x811c9dc5;
     while (*cp) {
         hash ^= (unsigned char) *cp++;
@@ -30,7 +29,7 @@ size_t fnv1a_hash(const char* cp)
     return hash;
 }
 
-size_t* hash_value(hash_t* hash, const char* clave){
+size_t *hash_value(hash_t *hash, const char *clave) {
     return fnv1a_hash(clave) % hash->m;
 }
 
@@ -40,12 +39,48 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato) {
         return NULL;
     }
 
+    hash->tabla = malloc(sizeof(hash_item_t) * CAPACIDAD_INICIAL);
+    for (int i = 0; i < CAPACIDAD_INICIAL; i++) {
+        hash->tabla[i] = lista_crear();
+    }
     hash->n = 0;
     hash->m = CAPACIDAD_INICIAL;
     hash->destruir_dato = destruir_dato;
     return hash;
 }
 
-size_t hash_cantidad(const hash_t *hash){
+void *hash_obtener(const hash_t *hash, const char *clave) {
+    size_t hash_key = hash_value(hash, clave);
+    lista_t *list = hash->tabla[hash_key];
+    if (list) {
+        lista_iter_t *iter = lista_iter_crear(list);
+        while (!lista_iter_al_final(iter) && strcmp(lista_iter_ver_actual(iter)->clave, clave) != 0) {
+            lista_iter_avanzar(iter);
+        }
+        if (lista_iter_al_final(iter)){
+            return NULL;
+        }
+        return lista_iter_ver_actual(iter)->valor;
+    }
+    return NULL;
+}
+
+void *hash_borrar(hash_t *hash, const char *clave) {
+    size_t hash_key = hash_value(hash, clave);
+    lista_t *list = hash->tabla[hash_key];
+    if (list) {
+        lista_iter_t *iter = lista_iter_crear(list);
+        while (!lista_iter_al_final(iter) && strcmp(lista_iter_ver_actual(iter)->clave, clave) != 0) {
+            lista_iter_avanzar(iter);
+        }
+        if (lista_iter_al_final(iter)){
+            return NULL;
+        }
+        return ((hash_item_t*)lista_iter_borrar(iter))->valor;
+    }
+    return NULL;
+}
+
+size_t hash_cantidad(const hash_t *hash) {
     return hash->n;
 }
