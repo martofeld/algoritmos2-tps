6a75e097e5c65afebfb39e1ee2453e7c26bf529f
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-10-09 13:09:35 -0300

    Descomento la ultima prueba

--- hash/pruebas_catedra.c
+++ hash/pruebas_catedra.c
@@ -411,7 +411,7 @@ void pruebas_hash_catedra()
     prueba_hash_valor_null();
     prueba_hash_volumen(5000, true);
     prueba_hash_iterar();
-    //prueba_hash_iterar_volumen(5000);
+    prueba_hash_iterar_volumen(5000);
 }
 
 void pruebas_volumen_catedra(size_t largo)
