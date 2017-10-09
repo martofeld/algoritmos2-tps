62b1bd33d158df75a3d21f945de5378342478120
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-10-09 11:25:36 -0300

    Creo los archivos de abb

---
+++ abb/abb.c
@@ -0,0 +1,16 @@
+//
+// Created by Martin Feldsztejn on 10/9/17.
+//
+
+#include "abb.h"
+
+typedef struct nodo nodo_t;
+struct nodo {
+    void* valor;
+    nodo_t* izq;
+    nodo_t* der;
+};
+
+struct abb {
+
+};
---
+++ abb/abb.h
@@ -0,0 +1,9 @@
+//
+// Created by Martin Feldsztejn on 10/9/17.
+//
+
+#ifndef ABB_ABB_H
+#define ABB_ABB_H
+
+typedef struct abb abb_t;
+#endif //ABB_ABB_H
