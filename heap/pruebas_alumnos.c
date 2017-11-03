073737e1ac8155a15ce5544e07a9707466c108aa
Mile <mile.marchese@hotmail.com>
2017-11-03 16:43:37 -0300

    Prueba heapsort

--- heap/pruebas_alumnos.c
+++ heap/pruebas_alumnos.c
@@ -4,6 +4,7 @@
 
 #include "../testing.c"
 #include "heap.h"
+#define VOLUMEN 500
 
 int comparar_ints(int a, int b){
     if (a == b){
@@ -30,6 +31,7 @@ void pruebas_encolar(){
     heap_encolar(heap, &array[8]);
     heap_encolar(heap, &array[0]);
     print_test("La cantidad es 7", heap_cantidad(heap) == 7);
+    print_test("El maximo es 8", heap_ver_max(heap) == &array[8]);
     heap_destruir(heap, NULL);
 }
 
@@ -80,10 +82,35 @@ void pruebas_volumen_heap(void){
     print_test("El heap fue destruido", true);
 }
 
+void pruebas_heapify(void){
+    int arreglo[10]={7,2,9,4,1,7,9,2,5,3};
+    heap_t* heap= heap_crear(comparar_void);
+    for(int i=0; i<10; i++){
+        heap_encolar(&array[i]);
+    }
+
+    
+
+}
+
+void pruebas_heapsort(void){
+    int array[]={9,3,4,6,1,2,5,7,8};
+    int array_o[]={1,2,3,4,5,6,7,8,9};
+    bool ok=true;
+    heapsort(array,9,comparar_void);
+    for(int i=0; i<9;i++){
+        ok&= array[i]==array_o[i];
+    }
+    print_test("El arreglo esta ordenado", ok);
+}
+
 
 void pruebas(){
     pruebas_encolar();
     pruebas_desencolar();
+    pruebas_volumen_heap();
+    pruebas_heapify();
+    pruebas_heapsort();
 }
 
 int main(){
