7999f703b1cc263050b3053496fa78eee388519d
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-11-06 11:28:36 -0300

    Arreglo errores

--- heap/pruebas_alumnos.c
+++ heap/pruebas_alumnos.c
@@ -123,7 +123,7 @@ void pruebas_heapsort() {
 }
 
 void pruebas_destruyendo(){
-    int largo = 10;
+    size_t largo = 10;
     heap_t* heap = heap_crear(comparar_void);
     int** array = malloc(sizeof(int*) * largo);
     for (int i = 0; i < largo; i++) {
