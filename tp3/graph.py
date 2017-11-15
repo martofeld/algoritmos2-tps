ffd0a09aaf89409202189fdd293622c57d565898
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-11-14 23:44:22 -0300

    Create basic files

---
+++ tp3/graph.py
@@ -0,0 +1,44 @@
+class Graph:
+    def __init__(self):
+        """"""
+        # TODO
+
+    def add_vertex(self, name):
+        """"""
+        # TODO
+
+    def remove_vertex(self, name):
+        """"""
+        # TODO
+
+    def add_edge(self, vertex1, vertex2, information, weight = 0):
+        """"""
+        # TODO
+
+    def remove_edge(self, vertex1, vertex2):
+        """"""
+        # TODO
+
+    def get_vertexes(self):
+        """"""
+        # TODO
+
+    def get_vertexes_count(self):
+        """"""
+        # TODO
+
+    def get_neighbours(self, vertex):
+        """"""
+        # TODO
+
+    def get_information(self, vertex1, vertex2):
+        """"""
+        # TODO
+
+    def __contains__(self, vertex):
+        """Checks if a vertex is in the graph"""
+        # TODO
+
+    def __iter__(self):
+        """Returns an iterator for the graph"""
+        # TODO
