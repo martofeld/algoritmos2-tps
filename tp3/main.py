71d5db87f457c8079d244d4829f6096e95d8c2a5
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-11-24 17:01:35 -0300

    Move dict below

--- tp3/main.py
+++ tp3/main.py
@@ -10,21 +10,10 @@ POPULARITY_VS = "popularidad_contra_KB"
 MOVIES_COUNT = "cantidad_peliculas"
 ACTORS_COUNT = "cantidad_actores"
 
-FUNCTIONS = {
-    PATH_TO_KB: path_to_kb,
-    BACON_NUMBER: bacon_number,
-    BACON_NUMBER_GT_6: bacon_number_gt_6,
-    BACON_NUMBER_INF: bacon_number_inf,
-    AVERAGE_KBN: average_kbn,
-    ACTORS_LIKE: actors_like,
-    POPULARITY_VS: popularity_vs,
-    MOVIES_COUNT: movies_count,
-    ACTORS_COUNT: actors_count,
-}
-
 
 def main():
     if len(sys.argv) != 2:
+        print("Wrong params")
         exit(1)
 
     create_graph(sys.argv[1])
@@ -39,7 +28,7 @@ def create_graph(file):
 def run_command():
     for line in sys.stdin:
         args = line.strip().split(' ')
-        FUNCTIONS[args[0]](*args[1:len(args)]) #Black magic
+        FUNCTIONS[args[0]](*args[1:len(args)])  # Black magic
 
 
 def path_to_kb():
@@ -85,3 +74,18 @@ def movies_count():
 def actors_count():
     """"""
     # TODO
+
+
+FUNCTIONS = {
+    PATH_TO_KB: path_to_kb,
+    BACON_NUMBER: bacon_number,
+    BACON_NUMBER_GT_6: bacon_number_gt_6,
+    BACON_NUMBER_INF: bacon_number_inf,
+    AVERAGE_KBN: average_kbn,
+    ACTORS_LIKE: actors_like,
+    POPULARITY_VS: popularity_vs,
+    MOVIES_COUNT: movies_count,
+    ACTORS_COUNT: actors_count,
+}
+
+main()
