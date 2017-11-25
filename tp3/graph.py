9ba0732f7fba65593998252319b41b3a9dec8057
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-11-25 13:13:35 -0300

    Change graph implementation

--- tp3/graph.py
+++ tp3/graph.py
@@ -1,27 +1,58 @@
+from collections import Set
+
+class _Edge:
+    def __init__(self, vertex1, vertex2, information = []):
+        self.vertex1 = vertex1
+        self.vertex2 = vertex2
+        self.information = information
+
+    def add_to_information(self, info):
+        self.information.append(info)
+
+    def get_key(self):
+        return set([self.vertex1, self.vertex2])
+
+    def has_vertex(self, vertex):
+        return vertex == self.vertex1 or vertex == self.vertex2
+
+    def __eq__(self, other):
+        return self.get_key() == other.get_key()
+
 class Graph:
     def __init__(self):
         """Creates a new empty graph"""
         self.vertexes = {}
+        self.edges = {}
 
     def add_vertex(self, name):
         """Adds a new vertex"""
-        self.vertexes[name] = {}
+        if not name in self.vertexes:
+            self.vertexes[name] = Set()
 
     def remove_vertex(self, name):
         """"""
         self.vertexes.pop(name)
-        for v in self.vertexes:
-            self.vertexes[v].pop(name)
+        for edge in self.edges:
+            if edge.has_vertex(name):
+                self.edges.pop(edge)
+
 
     def add_edge(self, vertex1, vertex2, information):
         """Adds a new edge between the two vertexes with the extra information"""
-        self.vertexes[vertex1][vertex2] = information
-        self.vertexes[vertex2][vertex1] = information
+        key = set([vertex2, vertex1])
+        if vertex2 in self.vertexes[vertex1]:
+            self.edges[key].add_to_information(information)
+        else:
+            self.vertexes[vertex1].append(vertex2)
+            self.vertexes[vertex2].append(vertex1)
+            self.edges[key] = _Edge(vertex1, vertex2, [information])
 
     def remove_edge(self, vertex1, vertex2):
         """Removes the edge that connects the two vertexes"""
-        self.vertexes[vertex1].pop(vertex2)
+        key = set([vertex1, vertex2])
         self.vertexes[vertex2].pop(vertex1)
+        self.vertexes[vertex1].pop(vertex2)
+        self.edges.pop(key)
 
     def get_vertexes(self):
         """"""
@@ -29,7 +60,12 @@ class Graph:
 
     def get_edges(self):
         """"""
-        # TODO
+        edges = set()
+        for edge in self.edges:
+            for info in edge.information:
+                edges.append(info)
+        return edges
+
 
     def get_vertexes_count(self):
         """Returns the amount of vertexes in the graph"""
@@ -37,11 +73,12 @@ class Graph:
 
     def get_neighbours(self, vertex):
         """Returns the neighbours of a given vertex"""
-        return self.vertexes[vertex].keys()
+        return self.vertexes[vertex]
 
     def get_information(self, vertex1, vertex2):
         """Returns the extra information from the edge of two vertexes"""
-        return self.vertexes[vertex1][vertex2]
+        key = set([vertex1, vertex2])
+        return self.edges[key].information
 
     def are_connected(self, vertex1, vertex2):
         """"""
