216760816a1daa1db35328cb3029d7edd59bc80b
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-11-26 15:24:36 -0300

    Muevo las cosas a donde corresponden

--- tp3/graph_functions.py
+++ tp3/graph_functions.py
@@ -8,7 +8,7 @@ def bfs(graph, starter_vertex):
     q.put((starter_vertex, 0))
     results.append((starter_vertex, 0))
     enqueued[starter_vertex] = True
-    while not q.is_empty():
+    while not q.empty():
         (u, pasos) = q.get()
         results.append((u, pasos))
         for w in graph.get_neighbours(u):
@@ -18,14 +18,38 @@ def bfs(graph, starter_vertex):
     return results
 
 
-def path(graph, actor1, actor2):
-    """"""
-    # TODO
+def path(graph, start, end):
+    if start not in graph or end not in graph:
+        return []
+    path=[]
+    path.append(start)
+    if start==end:
+        return path
+    visited={}
+    father={}
+    #obtener start
+    father[start]= None
+    visited[start]=True
+    process=Queue()
+    process.put(start)
+    found_end = False
+    while not process.empty() and not found_end:
+        v=process.get()
+        for w in graph.get_neighbours(v):
+            visited[w]=True
+            father[w]=v
+            process.put(w)
+            if w==end:
+                found_end = True
+                break
+    return make_path(father,start,end)
 
 
 def actors_at_distance(graph, actor, distance):
     """"""
-    # TODO
+    list= n_steps(graph,actor,distance)
+    list.sort()
+    return list
 
 
 def popularity(graph, actor):
@@ -36,3 +60,37 @@ def popularity(graph, actor):
 def similar(graph, actor):
     """"""
     # TODO
+
+def n_steps(graph, vertex, n):
+    visited = {}
+    level = {}
+    if vertex not in graph:
+        return False
+    #obtener el vertice
+    level[vertex]=0
+    process= Queue()
+    process.put(vertex)
+    visited[vertex]=True
+    lista=[]
+    while not process.empty():
+        v= process.get()
+        for w in graph.get_neighbours(v):
+            if w not in visited:
+                visited[w]=True
+                level[w]=level[v]+1
+                process.put(w)
+            if level[w]==n:
+                lista.append(w)
+    return lista
+
+def make_path(parents, start, end):
+    v=end
+    padre=parents[v]
+    lista=[]
+    while v != start:
+        lista.insert(1,v)
+        v=padre
+        padre=parents[v]
+
+def is_path(path, start, end):
+    return path[0]==start and path[-1]==end
